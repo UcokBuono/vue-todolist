95f10092d22a1d02152938b51716b31c15442fc9
NayanikaBuono <dwiyogabagoes@gmail.com>
2025-11-11 16:17:31 +0700

    add AppHeader

---
+++ fe-todo-list/src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="app-header">
+    <h1>😶‍🌫️Halo Dunia !</h1>
+  </header>
+</template>
+
+<style scoped>
+.appheader {
+  padding: 2rem, 1rem;
+  margin-bottom: 2rem;
+}
+
+h1 {
+  font-size: 2.5rem;
+  color: white;
+  font-weight: 700;
+  text-shadow: 2px 2px 4px rgba(0,0,0,0.1);
+  margin-bottom: 0.5rem;
+}
+</style>
